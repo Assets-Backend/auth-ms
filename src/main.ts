c4715808d2a2b7108559a212f3e4f494a2c13227
Ignacio Barros <barros_ignacio1714@yahoo.com>
2024-08-07 19:10:24 -0300

    Fix errors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,9 @@ async function bootstrap() {
 
     app.useGlobalPipes(
         new ValidationPipe({
+            whitelist: true,
             transform: true,
-            forbidNonWhitelisted: true,
+            // forbidNonWhitelisted: true,
         }),
     );
 
